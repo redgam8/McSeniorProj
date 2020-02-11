2c575d3665326441825da19f5e324e89ce3e9c26
jin <kimj155@mcmaster.ca>
2020-02-11 14:40:08 -0500

    Hash type changed to CHAR(64)

--- initdb.js
+++ initdb.js
@@ -3,13 +3,13 @@ var userDb = new sqlite3.Database("users.db");
 
 var password = "password";
 passwordSha = require('crypto').createHash('sha256').update(password).digest("hex");
-// console.log(passwordSha);
+// Consider putting salt?
 
 userDb.serialize(function(){
 
   // Create an initial table of users
   userDb.run("DROP TABLE IF EXISTS Users");
-  userDb.run("CREATE TABLE Users (username TEXT, passwordHash INT)");
+  userDb.run("CREATE TABLE Users (username TEXT, passwordHash CHAR(64))");
   userDb.run("INSERT INTO Users VALUES (?,?)", ['mem1', passwordSha]);
   userDb.run("INSERT INTO Users VALUES (?,?)", ['mem2', passwordSha]);
   userDb.run("INSERT INTO Users VALUES (?,?)", ['edit1', passwordSha]);
