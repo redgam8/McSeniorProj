a7265c53bfc7c35c7ec4229e71663fee2f90473b
Adolulu <redgam8@hotmail.com>
2020-02-25 20:58:20 -0500

    matching base

--- models/articles.js
+++ models/articles.js
@@ -1,6 +1,35 @@
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("database.db");
 
+function addRoute(route, username, role, time, callback){
+  // count existing records to calculate next ID
+  db.all("SELECT * from Route WHERE role= (?)", role,
+  function(err,results){
+    callback(results.length);
+  });
+
+  var ID = role + (results.length + 1);
+
+  // add the route with the ID
+  db.run("INSERT INTO Route VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+  [routeID, route.depLat, route.depLong, route.destLat, route.destLong, route.midpoint, username, time],
+  function(err){
+    callback();
+  });
+}
+
+
+
+
+
+function getRoute(route, username, time, callback){
+  var shortestPathID = ""
+}
+
+function match(driverID, riderID, driverPathID, riderPathID){
+
+}
+
 // Return all of the articles
 function getAllArticles(callback)
 {
@@ -21,6 +50,6 @@ function createArticle(article,username,callback)
 
 module.exports = {getAllArticles
                  ,createArticle};
-				 
-				 
+
+
    //db.run("CREATE TABLE Articles (ridetype TEXT, username TEXT, startinglocation TEXT, endlocation TEXT, status TEXT)");
