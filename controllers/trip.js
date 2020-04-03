d053695b33dff672099871793387bd0d008e7ca0
unknown <phuonc1@mcmaster>
2020-04-03 17:29:55 -0400

    latest code!

--- controllers/trip.js
+++ controllers/trip.js
@@ -8,59 +8,59 @@ var io = require('socket.io')(http);
 // Display the members page
 router.get("/", function (req, res)
 {
-	
-		/* req.session.fastestid
-		req.session.bingData2
-		req.session.testedid 
-		req.session.fastesttime */ 
-		
-		req.TPL.rideid = req.session.rideid
+
+	/* req.session.fastestid
+	req.session.bingData2
+	req.session.testedid
+	req.session.fastesttime */
+
+	req.TPL.rideid = req.session.rideid
 		req.TPL.userid = req.session.userid
 		req.TPL.ridetype = req.session.ridetype
 		req.TPL.startloc = req.session.startloc
 		req.TPL.startlong = req.session.startlong
 		req.TPL.startlat = req.session.startlat
 		req.TPL.endloc = req.session.endloc
-		req.TPL.endlong = req.session.endlong 
+		req.TPL.endlong = req.session.endlong
 		req.TPL.endlat = req.session.endlat
-		req.TPL.date = req.session.date 
-		req.TPL.time = req.session.time 
+		req.TPL.date = req.session.date
+		req.TPL.time = req.session.time
 		req.TPL.triptime = req.session.triptime
-		req.TPL.status = req.session.status 
+		req.TPL.status = req.session.status
+
+		console.log(req.session.fastesttripid);
+
+	res.render("trip", req.TPL);
+
+	let io = req.app.get("io");
+	io.on('connection', function (socket)
+	{
+		//socket.join('priv/John');
+		io.to('req.session.rideid').emit("assigneduserID",
+		{
+			assigneduserID: req.session.userid
+		}
+		);
+		//console.log("Logged in with userID");
+
+		console.log("Your Fastest ID is: " + req.session.fastesttripid);
+
+		io.to('/priv/John').emit("deliverquestion", req.session.fastesttripid);
+	}
+	);
+	/* io.on('connection', function(socket){
+	console.log("Question submitted: " + req.session.fastesttripid );
+
+	// Broadcast the question to all the students
+	socket.broadcast.emit("deliverquestion", req.session.fastesttripid);
+	socket.on("submitquestion", function()
+{
+
+	// Make sure we've received the question OK
 
 
-console.log(req.session.fastesttripid);
+	}); */
 
-
- res.render("trip", req.TPL);
-
-let io = req.app.get("io");
-          io.on('connection', function(socket){
-			 //socket.join('priv/John');
-          	io.to('req.session.rideid').emit("assigneduserID", {assigneduserID :req.session.userid});
-            //console.log("Logged in with userID");
-			
-			 console.log("Your Fastest ID is: " + req.session.fastesttripid );
-   
-    
-			io.to('/priv/John').emit("deliverquestion", req.session.fastesttripid);
-          });
-/* io.on('connection', function(socket){
-	 console.log("Question submitted: " + req.session.fastesttripid );
-   
-    // Broadcast the question to all the students
-    socket.broadcast.emit("deliverquestion", req.session.fastesttripid);
- socket.on("submitquestion", function()
-  {   
-
-    // Make sure we've received the question OK
-   
-
-  }); */
-  
-
-  
-  
-  
-});
+}
+);
 module.exports = router;
